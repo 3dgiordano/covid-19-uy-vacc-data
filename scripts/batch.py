bf016fea182b82bd2accf840da372d34581125f1
David <3dgiordano@gmail.com>
2021-04-09 16:20:09 -0300

    Export Segments and Regions

--- scripts/batch.py
+++ scripts/batch.py
@@ -1,12 +1,37 @@
 import pandas as pd
 
+base_seet = "https://docs.google.com/spreadsheets/d/" \
+            "1ktfI1cEm-VyvRbiFkXvzTixrDrCG-85Et9Clz69QBp8/gviz/tq?tqx=out:csv&sheet="
 
-def main():
+
+def uruguay():
     df = pd.read_csv(
-        "https://docs.google.com/spreadsheets/d/1ktfI1cEm-VyvRbiFkXvzTixrDrCG-85Et9Clz69QBp8/gviz/tq?tqx=out:csv&sheet=Uruguay"
+        f"{base_seet}Uruguay"
     )
 
     df.to_csv("../data/Uruguay.csv", index=False)
+
+
+def regions():
+    df = pd.read_csv(
+        f"{base_seet}Regions"
+    )
+
+    df.to_csv("../data/Regions.csv", index=False)
+
+
+def segments():
+    df = pd.read_csv(
+        f"{base_seet}Segment"
+    )
+
+    df.to_csv("../data/Segments.csv", index=False)
+
+
+def main():
+    uruguay()
+    regions()
+    segments()
 
 
 if __name__ == "__main__":
